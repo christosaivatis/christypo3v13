0c2b8e1d1112f91ae3b32a6352bfd5f922742539
Christos Aivatis <christos.aivatis@gmail.com>
2024-03-14 13:49:30 +0100

    Sitepackage created, added and installed

---
+++ mypackages/my_sitepackage/Resources/Public/JavaScript/Dist/scripts.js
@@ -0,0 +1,6 @@
+/*!
+ * My Sitepackage v1.0.0 (https://github.com/christosaivatis/christypo3v13)
+ * Copyright 2017-2024 Christos Aivatis
+ * Licensed under the GPL-2.0-or-later license
+ */
+console.log("WE LOVE TYPO3");
